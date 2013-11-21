b5efe5c9da0e487ffca63e84d354df99326c7f07
laurenbenichou <laurenbenichou@gmail.com>
2013-11-20 18:41:55 -0800

    fixed merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20131121011828) do
 
   create_table "quizzes", :force => true do |t|
@@ -35,7 +34,7 @@ ActiveRecord::Schema.define(:version => 20131121011828) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
-end
+  end
 
   create_table "users", :force => true do |t|
     t.string   "provider"
