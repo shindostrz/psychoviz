b3b79991d519d4efafc23e84ccbbfec75e11bcd9
Shindo N. Strzelczyk <shindo@Shindos-MacBook-Pro.local>
2013-11-22 13:52:57 -0800

    Fixed quiz logic, got the modals to work, added scores controller logic, and adjusted the index page

--- app/assets/javascripts/quiz.js
+++ app/assets/javascripts/quiz.js
@@ -19,24 +19,42 @@
 
 $(function(){
 
-  q = 0;
+  q = 1;
 
   var update_form;
 
-  $("#start_button").click(function(){
+  $("#start_button").click(function(e){
     $.get('/quiz.json').done(function(data){
       quiz = data["quiz"];
+      // index = (q-1);
       // $("#question_content").prepend(quiz[q]["question"]);
-      update_form = JST["templates/questions"](quiz[q]);
+      update_form = JST["templates/questions"](quiz[q-1]);
+      // console.log(index);
       $("#question_content").html(update_form);
-      $("#next").click(function(e){
-        e.preventDefault();
-        if (q < 70) {
-          q++;
-          update_form = JST["templates/questions"](quiz[q]);
-          $("#question_content").html(update_form);
-        }
-      });
-      });
     });
+  });
+
+$("#next").click(function(e){
+  if ($(".answer1").is(":checked") === false && $(".answer2").is(":checked") === false) {
+      e.preventDefault();
+      alert("Please choose one answer");
+  } else {
+      e.preventDefault();
+      $(".md-close").click();
+      $(".md-trigger").click();
+      answer_a = $("#answer_a").val();
+      answer_b = $("#answer_b").val();
+      app.score(q, answer_a);
+      q++;
+      if (q-1 < 70) {
+        update_form_again = JST["templates/questions"](quiz[q-1]);
+        $("#question_content").html(update_form_again);
+      } else {
+        $.post( "/scores", { e: app.e, f: app.f, i: app.i, j: app.j, n: app.n, p: app.p, s: app.s, t: app.t } ).done(function(){
+        $('#some_div').append('#chart_div');
+        $(".md-close").click();
+        });
+      }
+    }
+  });
 });
--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,2 +1,12 @@
 class ScoresController < ApplicationController
+
+  def create
+        result = []
+        result.push(e, f, i, j, n, p, s, t)
+    binding.pry
+        type = result.sort!.slice(4..7)
+        user = current_user
+        user.scores.create(e: e, f: f, i: i, j: j, n: n, p: p, s: s, t: t, type: type)
+  end
+
 end
