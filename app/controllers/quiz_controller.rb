5a337608f8827d3da93dfc8e65672040c885155b
Imogen <imogen.wentworth@gmail.com>
2013-11-21 17:31:52 -0800

    Trying to make JSON work

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -2,7 +2,6 @@ class QuizController < ApplicationController
 
   def index
     @questions = Quiz.all
-
     respond_to do |format|
       format.html # index.html.erb
       format.json { render :json => { quiz: @questions }}
