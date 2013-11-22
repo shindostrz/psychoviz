f69a875fee4f7eca5243b42c1745e7d78d2c466c
Imogen <imogen.wentworth@gmail.com>
2013-11-22 00:19:37 -0800

    Get data from JS through AJAX

--- app/assets/javascripts/quiz.js
+++ app/assets/javascripts/quiz.js
@@ -21,9 +21,16 @@ $(function(){
 
   $("#start_button").click(function(){
     $.get('/quiz.json').done(function(data){
-      console.log(data)
+      console.log(data);
     });
-  })
+  });
+
+  $("#end_button").click(function(){
+    $.post( "/", { e: e, f: f, i: i, j: j, n: n, p: p, s: s, t: t } ).done(function(){
+      $('#some_div').append('#chart_div');
+      $(".md-close").click();
+    });
+  });
 
   $("#next").click(function(){
     $(".md-close").click();
@@ -33,3 +40,4 @@ $(function(){
   });
 
 });
+
