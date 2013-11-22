7e40730a80d6ad7dbba87a1bb35da39150107dfb
laurenbenichou <laurenbenichou@gmail.com>
2013-11-21 17:08:42 -0800

    started working on ajax

--- app/assets/javascripts/quiz.js
+++ app/assets/javascripts/quiz.js
@@ -1,24 +1,38 @@
-$(document).ready(function(){
-    $('#start_button').on('click', function(){
-        $.ajax({
-            url: '/quiz',
-            method: 'GET'
-        }).done(function(data){
-            console.log(data);
-            var question = data;
-            var question1 = question[:q_num][1]
-              var update_form;
-              update_form = JST["templates/questions"](question1);
-              return $("#question_content").append(update_form);
-        });
-    });
-});
+// $(document).ready(function(){
+//     $('#start_button').on('click', function(event){
+//         debugger
+//         event.preventDefault();
+//         $(".md-trigger").click();
+//         $.ajax({
+//             url: '/quiz',
+//             method: 'GET'
+//         }).done(function(data){
+//             console.log(data);
+//             var question = data;
+//             var question1 = question[:q_num][1]
+//               var update_form;
+//               update_form = JST["templates/questions"](question1);
+//               return $("#question_content").append(update_form);
+//         });
+//     });
+// });
 
 $(function(){
+
   $("#next").click(function(){
     $(".md-close").click();
     setTimeout(function(){
       $(".md-trigger").click();
     }, 500);
   });
+
+  $("#start_button").click(function(event){
+    event.preventDefault(event)
+    $.getJSON({
+      url: '/quiz.json',
+    }).done(function(data){
+      console.log(data)
+    })
+
+  })
 });
