c2a24b308e69c82e95c898e661127f11eb340888
Shindo Strzelczyk <shindostrz@gmail.com>
2014-06-22 23:31:59 -0700

    Added a controller method to get friends from the Fb graph api and find those who are in the database

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -8,8 +8,21 @@ class QuizController < ApplicationController
     end
   end
 
-  def show
+  def get_friends
+    @graph = Koala::Facebook::API.new(current_user.oauth_token)
+    all_friends = @graph.get_connections("me", "friends")
+    user_friends = []
+    all_friends.each do |friend|
+      if User.find_by_uid(friend["id"])
+        user_friends << friend
+      end
+      user_friends
+    end
 
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => { friends: user_friends }}
+    end
 
   end
 
