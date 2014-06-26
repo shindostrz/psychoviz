1d1ea48eeb3e926a59a95e4a0c147402eb809eb2
Shindo Strzelczyk <shindostrz@gmail.com>
2014-06-25 18:30:07 -0700

    Added logic to the from_omniauth method which exchanges a short-term token for a long-term one

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ActiveRecord::Base
   has_one :score
 
   def self.from_omniauth(auth)
+
+    oauth = Koala::Facebook::OAuth.new(ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"])
+    new_access_info = oauth.exchange_access_token_info auth.credentials.token
+
+    new_access_token = new_access_info["access_token"]
+    new_access_expires_at = DateTime.now + new_access_info["expires"].to_i.seconds
+
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
@@ -11,10 +18,14 @@ class User < ActiveRecord::Base
       user.image = auth.info.image
       user.location = auth.info.location
       user.timezone = auth.extra.timezone
-      user.oauth_token = auth.credentials.token
-      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.oauth_token = new_access_token
+      user.oauth_expires_at = new_access_expires_at
       user.save!
     end
+  end
+
+  def facebook
+    @facebook ||= Koala::Facebook::API.new(oauth_token)
   end
 
 end
