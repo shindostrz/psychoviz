71970c2468b026e39512b3c471d96279dd6a1575
Shindo Strzelczyk <shindostrz@gmail.com>
2014-06-28 00:38:39 -0700

    Changed personalityType variable name to match the property from the ajax function.

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -2,7 +2,7 @@ class ScoresController < ApplicationController
   def create
     results_hash = params[:data]
     user = current_user
-    user.create_score(e: results_hash[:e], f: results_hash[:f], i: results_hash[:i], j: results_hash[:j], n: results_hash[:n], p: results_hash[:p], s: results_hash[:s], t: results_hash[:t], personality_type: results_hash[:personality_type].join(",").gsub(',', ''))
+    user.create_score(e: results_hash[:e], f: results_hash[:f], i: results_hash[:i], j: results_hash[:j], n: results_hash[:n], p: results_hash[:p], s: results_hash[:s], t: results_hash[:t], personality_type: results_hash[:personalityType].join(",").gsub(',', ''))
     render :nothing => true
   end
 
