8248f49c3ead3556b2bff45daf3686e3b8def4f7
Shindo Strzelczyk <shindostrz@gmail.com>
2014-07-03 14:44:55 -0700

    Added base64 encoding and functions to share the canvas image to Facebook without saving first.

---
+++ app/assets/javascripts/facebookImageShare.js
@@ -0,0 +1,46 @@
+if ( XMLHttpRequest.prototype.sendAsBinary === undefined ) {
+    XMLHttpRequest.prototype.sendAsBinary = function(string) {
+        var bytes = Array.prototype.map.call(string, function(c) {
+            return c.charCodeAt(0) & 0xff;
+        });
+        this.send(new Uint8Array(bytes));
+    };
+};
+
+function postImageToFacebook( authToken, filename, mimeType, imageData, message )
+{
+    // this is the multipart/form-data boundary we'll use
+    var boundary = '----ThisIsTheBoundary1234567890';
+    // let's encode our image file, which is contained in the var
+    var formData = '--' + boundary + '\r\n'
+    formData += 'Content-Disposition: form-data; name="source"; filename="' + filename + '"\r\n';
+    formData += 'Content-Type: ' + mimeType + '\r\n\r\n';
+    for ( var i = 0; i < imageData.length; ++i )
+    {
+        formData += String.fromCharCode( imageData[ i ] & 0xff );
+    }
+    formData += '\r\n';
+    formData += '--' + boundary + '\r\n';
+    formData += 'Content-Disposition: form-data; name="message"\r\n\r\n';
+    formData += message + '\r\n'
+    formData += '--' + boundary + '--\r\n';
+
+    var xhr = new XMLHttpRequest();
+    xhr.open( 'POST', 'https://graph.facebook.com/me/photos?access_token=' + authToken, true );
+    xhr.onload = xhr.onerror = function() {
+        console.log( xhr.responseText );
+    };
+    xhr.setRequestHeader( "Content-Type", "multipart/form-data; boundary=" + boundary );
+    xhr.sendAsBinary( formData );
+};
+
+function postCanvasToFacebook() {
+  var canvas = document.getElementById("myChart");
+  var data = canvas.toDataURL("image/png");
+  var encodedPng = data.substring(data.indexOf(',') + 1, data.length);
+  var decodedPng = Base64Binary.decode(encodedPng);
+  FB.login(function(response) {
+    console.log(response);
+    postImageToFacebook(response.authResponse.accessToken, "psychvizchart", "image/png", decodedPng, "My PyschViz personality results!");
+   }, {scope: "publish_actions"});
+}
