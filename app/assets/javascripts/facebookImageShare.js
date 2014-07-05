1ab250976bd294f242926af3b31e55311e01e76c
Shindo Strzelczyk <shindostrz@gmail.com>
2014-07-05 13:24:43 -0700

    Added logic to post the hidden canvas if the window width is less than 768

--- app/assets/javascripts/facebookImageShare.js
+++ app/assets/javascripts/facebookImageShare.js
@@ -5,14 +5,14 @@ if ( XMLHttpRequest.prototype.sendAsBinary === undefined ) {
         });
         this.send(new Uint8Array(bytes));
     };
-};
+}
 
 function postImageToFacebook( authToken, filename, mimeType, imageData, message )
 {
     // this is the multipart/form-data boundary we'll use
     var boundary = '----ThisIsTheBoundary1234567890';
     // let's encode our image file, which is contained in the var
-    var formData = '--' + boundary + '\r\n'
+    var formData = '--' + boundary + '\r\n';
     formData += 'Content-Disposition: form-data; name="source"; filename="' + filename + '"\r\n';
     formData += 'Content-Type: ' + mimeType + '\r\n\r\n';
     for ( var i = 0; i < imageData.length; ++i )
@@ -22,7 +22,7 @@ function postImageToFacebook( authToken, filename, mimeType, imageData, message 
     formData += '\r\n';
     formData += '--' + boundary + '\r\n';
     formData += 'Content-Disposition: form-data; name="message"\r\n\r\n';
-    formData += message + '\r\n'
+    formData += message + '\r\n';
     formData += '--' + boundary + '--\r\n';
 
     var xhr = new XMLHttpRequest();
@@ -32,14 +32,20 @@ function postImageToFacebook( authToken, filename, mimeType, imageData, message 
     };
     xhr.setRequestHeader( "Content-Type", "multipart/form-data; boundary=" + boundary );
     xhr.sendAsBinary( formData );
-};
+}
 
 function postCanvasToFacebook(message) {
-  var canvas = document.getElementById("myChart");
-  var data = canvas.toDataURL("image/png");
-  var encodedPng = data.substring(data.indexOf(',') + 1, data.length);
-  var decodedPng = Base64Binary.decode(encodedPng);
-  FB.login(function(response) {
+    var canvas;
+    // This if statement might cause a blank canvas on resize, but we can deal with that later
+    if (Quiz.mobile === true) {
+        canvas = document.getElementById("hiddenCanvas");
+    } else {
+        canvas = document.getElementById("myChart");
+    }
+    var data = canvas.toDataURL("image/png");
+    var encodedPng = data.substring(data.indexOf(',') + 1, data.length);
+    var decodedPng = Base64Binary.decode(encodedPng);
+    FB.login(function(response) {
     postImageToFacebook(response.authResponse.accessToken, "psychvizchart", "image/png", decodedPng, message);
-   }, {scope: "publish_actions"});
+    }, {scope: "publish_actions"});
 }
